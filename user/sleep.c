81b546f85b945ed398ffcd9a2331dfdcdc5809bf
hitlzm <hitlzm@163.com>
2026-03-06 18:38:22 +0800

    space2

--- user/sleep.c
+++ user/sleep.c
@@ -7,10 +7,9 @@ int main(int argc, char *argv[])
     fprintf(2, "usage: sleep [ticks num]\n");
     exit(1);
   }
-
-
   // atoi sys call guarantees return an integer
   int ticks = atoi(argv[1]);
+
   int ret = sleep(ticks);
   exit(ret);
 }

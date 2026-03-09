aa8172edc74f8969af70a9ac1d0a0a6a94c0e682
hitlzm <hitlzm@163.com>
2026-03-09 15:03:15 +0800

    primeschange

--- user/primes.c
+++ user/primes.c
@@ -1,66 +1,56 @@
 #include "kernel/types.h"
 #include "user/user.h"
 
-/*
- * Run as a prime-number processor
- * the listenfd is from your left neighbor
- */
-void runprocess(int listenfd) {
-  int my_num = 0;
-  int forked = 0;
-  int passed_num = 0;
-  int pipes[2];
-  while (1) {
-    int read_bytes = read(listenfd, &passed_num, 4);
+void sieve(int pipefd[2]);
 
-    // left neighbor has no more number to provide
-    if (read_bytes == 0) {
-      close(listenfd);
-      if (forked) {
-        // tell my children I have no more number to offer
-        close(pipes[1]);
-        // wait my child termination
-        int child_pid;
-        wait(&child_pid);
-      }
-      exit(0);
-    }
+int main(){
+    int pipefd[2];
 
-    // if my initial read
-    if (my_num == 0) {
-      my_num = passed_num;
-      printf("prime %d\n", my_num);
-    }
+    pipe(pipefd);
 
-    // not my prime multiple, pass along
-    if (passed_num % my_num != 0) {
-      if (!forked) {
-        pipe(pipes);
-        forked = 1;
-        int ret = fork();
-        if (ret == 0) {
-          // i am the child
-          close(pipes[1]);
-          close(listenfd);
-          runprocess(pipes[0]);
-        } else {
-          // i am the parent
-          close(pipes[0]);
+    if(fork() == 0){
+        close(pipefd[1]);
+        sieve(pipefd);
+    } else {
+        close(pipefd[0]);
+
+        for(int i = 2; i <= 35; i++){
+            write(pipefd[1], &i, sizeof(int));
         }
-      }
-      // pass the number to right neighbor
-      write(pipes[1], &passed_num, 4);
+
+        close(pipefd[1]);
+        wait(0);
     }
-  }
+
+    exit(0);
 }
 
-int main(int argc, char *argv[]) {
-  int pipes[2];
-  pipe(pipes);
-  for (int i = 2; i <= 35; i++) {
-    write(pipes[1], &i, 4);
-  }
-  close(pipes[1]);
-  runprocess(pipes[0]);
-  exit(0);
+
+void sieve(int pipefd[2]) {
+    int prime;
+    int num;
+    int newpipe[2];
+
+    if(read(pipefd[0], &prime, sizeof(int)) == 0)
+        exit(0);
+
+    printf("prime %d\n", prime);
+
+    pipe(newpipe);
+
+    if(fork() == 0){
+        close(newpipe[1]);
+        sieve(newpipe);
+    } else {
+        close(newpipe[0]);
+
+        while(read(pipefd[0], &num, sizeof(int)) != 0){
+            if(num % prime != 0){
+                write(newpipe[1], &num, sizeof(int));
+            }
+        }
+
+        close(newpipe[1]);
+        wait(0);
+    }
 }
